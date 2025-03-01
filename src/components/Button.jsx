1cb5041832880a5c26496ffccadad22ca1d6d5f7
Aditya Singh <adityaa9170843@gmail.com>
2025-03-01 21:13:19 +0530

    reusable button created

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ButtonSvg from "../assets/svg/ButtonSvg";
+import "./button.css";
+
 function Button({ className, href, onClick, children, px, white }) {
-  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
-    px || "px-7"
-  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
-  const spanClasses =" relative z-10"
+  
   return (
-    <button className={classes}>
-      <span className={spanClasses}>{children}</span>
+    <button className="button">
+      <span>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
