ec6e684ab67a10e99d243d68dc6a5b6174313b84
Aditya Singh <adityaa9170843@gmail.com>
2025-03-01 15:15:47 +0530

    button added

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ButtonSvg from "../assets/svg/ButtonSvg";
+function Button({ className, href, onClick, children, px, white }) {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const spanClasses =" relative z-10"
+  return (
+    <button className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+}
+
+export default Button;
